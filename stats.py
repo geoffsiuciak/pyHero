d48f957d535c59d85e5a5d4aa7d1d382d4832164
geoff-siuciak <60087423+geoff-siuciak@users.noreply.github.com>
2020-08-11 03:04:19 -0500

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,22 @@
+#
+# OBJECT CONTAINING REAL-TIME GAME STATS AND RELEVANT METHODS
+#
+
+
+class Stats:
+    def __init__(self, note_count):
+        self.score = 0
+        self.streak = 0
+        self.total_hit = 0
+        self.high_streak = 0
+        self.combo = False
+        self.note_count = note_count
+
+    def getPercent(self):
+        percent_score = self.total_hit / self.note_count
+        return percent_score
+
+    def recordStreak(self):
+        if self.streak > self.high_streak:
+            self.high_streak = self.streak
+            self.streak = 0
