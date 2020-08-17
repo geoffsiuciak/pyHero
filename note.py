87b3f7e6dc2e0513d35f52aa0bbb09a6d4fbaf76
geoff-siuciak <60087423+geoff-siuciak@users.noreply.github.com>
2020-08-17 05:26:42 -0500

    Update note.py

--- note.py
+++ note.py
@@ -1,19 +1,26 @@
 #
 # --- NOTE CLASS ---
-# INIT EACH NOTE OBJECT USING ATTRIBUTE LIST READ
-# FROM EACH LINE OF EXCEL FILE WITH PANDAS DATA FRAME
+# init 1 per excel entry
 #
 
 
 class Note:
-    def __init__(self, NOTE_DATA):  # n=3 LIST OF BELOW DATA OBJECTS
+    def __init__(self, NOTE_DATA):  # JSON->LIST
 
-        self.NOTE_TYPE = NOTE_DATA[0]  # LIST OF 5 BOOL VALUES, TRUE FOR EACH NOTE IN OBJECT
-        self.SUSTAIN_LENGTH = NOTE_DATA[1]  # LENGTH OF SUSTAIN IN PIXELS (CONVERTED FROM SEC)
-        self.TIME_STAMP = NOTE_DATA[2]  # PRECISE TIME STAMP OF NOTE IN SONG (FLOAT - SEC TO FRAMES)
+        self.NOTE_TYPE = NOTE_DATA[0]  # 0,0,0,0,0
+        self.SUSTAIN_LENGTH = NOTE_DATA[1]  # durationTicks
+        self.TIME_STAMP = NOTE_DATA[2]  # ticks to ms
 
         # TO-DO FOR CONTROLLER SUPPORT - ADD HO/PO
         # self.is_HOPO = NOTE_DATA[1]  # TRUE IF HAMMER-ON/PULL-OFF (NO STRUM REQUIRED)
 
-        self.Y_POS = -264  # 1000 px TO CENTER OF HIT AREA
-        self.HIT = False
+        self.Y_POS = -264
+        self.MISSED_Y_POS = 736
+
+    # RETURN LIST OF IMAGE INDICES BASED ON ARRAY IN self.NOTE_TYPE
+    def getAnchors(self):
+        images = []
+        for index, note_type in enumerate(self.NOTE_TYPE):
+            if int(note_type) == 1:
+                images.append(index)
+        return images
