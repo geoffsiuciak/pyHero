6025c901a01d9cd8b2604917762a9735b4d2028f
geoff-siuciak <60087423+geoff-siuciak@users.noreply.github.com>
2020-08-17 05:25:45 -0500

    Update pyHero.py

--- pyHero.py
+++ pyHero.py
@@ -1,16 +1,11 @@
 import pygame
-import pygame
-import os
-import random
-import time
-
 import data
 import actions
 from stats import Stats
 
 # INIT PYGAME, ASSETS
 pygame.init()
-pygame.mixer.init()
+pygame.mixer.pre_init(44100, -16, 2, 1024)
 pygame.font.init()
 clock = pygame.time.Clock()
 SCREEN = pygame.display.set_mode((data.WIDTH, data.HEIGHT))
@@ -18,9 +13,11 @@ pygame.display.set_caption(data.CAPTION)
 actions.loadAssets()
 
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - -   - - - - - - - - - - - - - - - - - - - - - - - - -
 # STARTUP SCREEN
 def startScreen():
+    # actions.parseJson()
+    # print(pygame.font.get_fonts())
     start_screen = True
 
     while start_screen:
@@ -48,9 +45,14 @@ def menuScreen():
         actions.drawMenuScreen(SCREEN)
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_d]:
+        if keys[pygame.K_1]:
             start_screen = False
-            playSong(data.SONG_1)  # return from function
+            song = data.SONG_1
+            playSong(song)
+        if keys[pygame.K_2]:
+            start_screen = False
+            song = data.SONG_2
+            playSong(song)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
@@ -61,18 +63,40 @@ def menuScreen():
 # SONG PLAY THROUGH LOOP
 def playSong(song):
     # LOAD NOTE OBJECTS INTO LIST
-    NOTES, notes_onScreen = actions.MIDItoList(song)
+    NOTES, notes_onScreen, missed_notes, sustain_bars = actions.MIDItoList(song)
     game_stats = Stats(len(NOTES))
 
     playing = True
-    actions.engageButtons(SCREEN)
+    actions.engageAssets(SCREEN)
+    actions.startAudio(song[1])
+
     while playing:
         clock.tick(data.FPS)
         actions.drawStaticAssets(SCREEN)
         inputs = actions.getInputs(SCREEN)
-        actions.noteEngine(SCREEN, NOTES, notes_onScreen, inputs, game_stats)
+        actions.noteEngine(SCREEN,
+                           NOTES,
+                           notes_onScreen,
+                           inputs,
+                           game_stats,
+                           missed_notes,
+                           sustain_bars)
+        actions.getInputs(SCREEN)
         actions.updateSCREEN()
+
+        # print(notes_onScreen)
+        # print(NOTES)
+
+        # print(game_stats.score)
+
     pygame.quit()
+    game_stats.getGame()
+
+
+def postGame():
+    # SHOW STATS
+    # RETURN TO SONG SELECT
+    pass
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
